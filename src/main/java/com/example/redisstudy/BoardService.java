88a67579aee6d9d1b6cabad83b063fc03e1ff2cb
Junsik <junsik213@naver.com>
2024-08-26 04:04:02 +0900

    Redis 세팅

--- src/main/java/com/example/redisstudy/BoardService.java
+++ src/main/java/com/example/redisstudy/BoardService.java
@@ -1,5 +1,6 @@
 package com.example.redisstudy;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,12 +10,14 @@ import java.util.List;
 
 @Service
 public class BoardService {
+
     private BoardRepository boardRepository;
 
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
 
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
